56b1f0a8dcd22ef1dbc422bac8b78a4ac4967769
Jade Stewart <jadekstewart3@gmail.com>
2023-05-02 14:58:44 -0600

    test: remove host validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
  describe "validations" do 
   it { should validate_presence_of :name }
   it { should validate_presence_of :email }
-  it { should validate_presence_of :host }
   it { should validate_uniqueness_of :email }
   it { should validate_presence_of :password_digest }
   it { should have_secure_password }
