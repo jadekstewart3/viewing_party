d3dc1cad115281ae20937577c2aa3975e2376201
Jade Stewart <jadekstewart3@gmail.com>
2023-05-02 14:57:48 -0600

    feat: add routes for users dashboard and register

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "welcome#index"
+
+  resources :users, only: [:create]
+
+  get "/dashboard", to: "users#show"
+  get "/register", to: "users#new"
 end
