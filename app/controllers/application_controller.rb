72f697b4ba67aa0ce6d361ed84afe29813b2b3b8
Jade Stewart <jadekstewart3@gmail.com>
2023-05-02 14:55:47 -0600

    feat:Create index action in welcome controller, and new, create, and show actions in Users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+  add_flash_types :error, :success
+
+  def current_user
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+  def error_message(errors)
+    errors.full_messages.join(", ")
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params  
+    user[:email] = user[:email].downcase
+    new_user = User.create(user)
+    session[:user_id] = new_user.id
+    if new_user.save
+      redirect_to dashboard_path
+    else
+      redirect_to "/register"
+      flash[:error] = error_message(new_user.errors)
+    end 
+  end
+
+  def show
+    if current_user
+     @user = current_user
+    else
+      flash[:error] = "You must be logged in to access your dashboard."
+      redirect_to root_path
+    end
+  end
+
+  private
+   def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
