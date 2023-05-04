80015ae66e5cb3675165781a23d1fd585a7fe310
Jade Stewart <jadekstewart3@gmail.com>
2023-05-04 16:39:01 -0600

    test: complete testing for movie facade

--- spec/facades/movie_facade_spec.rb
+++ spec/facades/movie_facade_spec.rb
@@ -1,5 +1,57 @@
 require "rails_helper"
 
 RSpec.describe MovieFacade do 
-  
+  describe "#instance methods" do 
+    describe "get_top_rated_movies", :vcr do 
+      it "returns a list of 20 top rated movies" do 
+
+        top_movies = MovieFacade.new({}).get_top_rated_movies
+      
+        top_movies.each do |movie|
+          expect(movie).to be_a(Movie)
+          expect(movie.title).to be_a(String)
+          expect(movie.vote_average).to be_a(Float)
+          expect(movie.runtime).to be(nil)
+          expect(movie.genres).to be(nil)
+          expect(movie.summary).to be(nil)
+          expect(movie.cast).to be(nil)
+          expect(movie.total_reviews).to be(nil)
+          expect(movie.review_info).to be(nil)
+        end
+      end
+    end
+
+    describe "get_searched_movies", :vcr do 
+      it "returns a list of searched movies" do
+        searched_movies = MovieFacade.new(search: "Ponyo").get_searched_movies
+
+        searched_movies.each do |movie|
+          expect(movie).to  be_a(Movie)
+          expect(movie.title).to be_a(String)
+          expect(movie.vote_average).to be_a(Float)
+          expect(movie.runtime).to be(nil)
+          expect(movie.genres).to be(nil)
+          expect(movie.summary).to be(nil)
+          expect(movie.cast).to be(nil)
+          expect(movie.total_reviews).to be(nil)
+          expect(movie.review_info).to be(nil)
+        end
+      end
+    end
+
+    describe "get_movie_details", :vcr do 
+      it "returns a movies full details" do 
+        movie = MovieFacade.new(movie_id: 129).get_movie_details
+
+        expect(movie.title).to be_a(String)
+        expect(movie.vote_average).to be_a(Float)
+        expect(movie.runtime).to be_an(Integer)
+        expect(movie.genres).to be_an(Array)
+        expect(movie.summary).to be_a(String)
+        expect(movie.cast).to be_an(Array)
+        expect(movie.total_reviews).to be_an(Integer)
+        expect(movie.review_info).to be_an(Array)
+      end
+    end
+  end
 end
