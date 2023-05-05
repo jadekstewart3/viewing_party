9a284b19066fc04d340dbe1bc57707a23fc03742
Jade Stewart <jadekstewart3@gmail.com>
2023-05-05 11:06:34 -0600

    test: test movie show page

--- spec/features/users/movies/show_spec.rb
+++ spec/features/users/movies/show_spec.rb
@@ -7,8 +7,15 @@ RSpec.describe "Movie Details Page", :vcr do
         @phil = User.create!(name: "Philip", email: "philipjfry@gmail.com", password: "iloveclimbing", host: true)
         visit user_movie_path(@phil, 129)
       end
-      it "displays a button to create a new viewing party" do 
-        
+      it "displays the movie's attributes" do 
+        expect(page).to have_content("Spirited Away")
+        expect(page).to have_content("Summary:")
+        expect(page).to have_content("Runtime:")
+        expect(page).to have_content("Vote Average:")
+        expect(page).to have_content("Genres:")
+        expect(page).to have_content("Cast:")
+        expect(page).to have_content("Reviews:")
+        expect(page).to have_content("Total Reviews:")
       end
     end
   end
